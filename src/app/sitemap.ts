f6d42693612cc41c27f1e2643198f35746bd9fdd
Mirza Jaffar Abbas <78757044+Jaffar94@users.noreply.github.com>
2026-08-16 01:05:46 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://kagazkit.site'; // Replace with actual domain later
+  const baseUrl = 'https://www.kagazkit.site';
 
   return [
     {
